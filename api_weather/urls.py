e86dc3ef9887a9d85f661e7848d814bd6defb965
lucianlorens <lucianlorens@gmail.com>
2020-12-03 00:13:43 -0300

    remove env

--- api_weather/urls.py
+++ api_weather/urls.py
@@ -5,10 +5,12 @@ from api_weather.control_panel import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+#router.register(r'status', views.StatusViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))#,
+    #path('status/', include())
 ]
