077871910a658118ffed262de21682ab69500eb7
lucianlorens <lucianlorens@gmail.com>
2020-12-15 01:43:51 -0300

    :card_file_box: add basic Location model

--- api_weather/control_panel/models.py
+++ api_weather/control_panel/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
-#class Location(models.Model):
+class Location(models.Model):
+    location_id = models.CharField=(max_length=50)
+    description = models.CharField=(max_length=50) 
+    parameters = models.CharField=(max_length=50)
 
+    class Meta:
+        verbose_name_plural = 'locations'
+    
+    def __str(self):
+        return self.title
